ace8ee2e4cb7a22e8aa14e4098973eb57742adc2
L F <logan.fink@dgs.virginia.gov>
2025-02-04 20:41:05 +0000

    GC content % to fastq-peek

--- bin/fastq-peek.sh
+++ bin/fastq-peek.sh
@@ -26,3 +26,30 @@ LINE_COUNT=$(wc -l < "$FASTQ_FILE")
 READ_COUNT=$((LINE_COUNT / 4))
 
 echo "Number of reads in $FASTQ_FILE: $READ_COUNT"
+
+# Count number of G and C nucleotides
+# Initialize COUNT variables
+GC_COUNT=0
+ALL_NUCLEOTIDES_COUNT=0
+
+##Go through fastq line by line
+while IFS= read -r line; do 
+    ##Find the lines that contain alphabetical letters,
+    ##but does not start with @
+    if [[ $line == [A-Za-z]* && $line != "@"* ]]; then
+        #Count number of letters in line
+        NUCLEOTIDE_per_LINE=$(($(echo $line| wc -c) - 1))
+        ALL_NUCLEOTIDES_COUNT=$(($NUCLEOTIDE_per_LINE +  $ALL_NUCLEOTIDES_COUNT))
+        #Count number G's in line
+        G_COUNT=$(echo $line| grep -o -i "G" | wc -l)
+        #Count number of C's in line
+        C_COUNT=$(echo $line| grep -o -i "C" | wc -l)
+        GC_COUNT=$(($GC_COUNT + $G_COUNT + $C_COUNT))
+    fi;
+done < $FASTQ_FILE
+
+#Calculate GC content
+GC_PERCENT=$(awk "BEGIN { print $GC_COUNT / $ALL_NUCLEOTIDES_COUNT }")
+GC_PERCENTAGE=$(awk "BEGIN { print $GC_PERCENT * 100 }")
+
+echo "GC content in $FASTQ_FILE: ${GC_PERCENTAGE}%"
